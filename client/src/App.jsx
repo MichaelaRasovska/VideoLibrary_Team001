fcfdc2234a0f5fdaa076ddf91f775cea1641c4db
MichaelaRasovska <michaela.rasovska@gmail.com>
2023-04-30 22:48:01 +0200

    napojení na server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,33 @@
-import './App.css';
+import styles from './App.css';
 import VideoList from './bricks/VideoList';
-import videoData from './data/videoData';
 import Header from './bricks/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Icon from '@mdi/react';
+import { mdiLoading } from '@mdi/js';
 
 function App() {
+  //server call
+  const [videosLoadCall, setVideosLoadCall] = useState({
+    state: 'pending',
+  });
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/videos`, {
+      method: 'GET',
+    }).then(async (response) => {
+      const responseJson = await response.json();
+      if (response.status >= 400) {
+        setVideosLoadCall({ state: 'error', error: responseJson });
+      } else {
+        setVideosLoadCall({ state: 'success', data: responseJson });
+      }
+    });
+  }, []);
+
+  const videoData = videosLoadCall.data;
+
+  //re render based on new data
   const [newData, setNewData] = useState([]);
   const [newEntry, setNewEntry] = useState(false);
 
@@ -14,16 +36,39 @@ function App() {
     setNewEntry(true);
   };
 
-  return (
-    <div className="App">
-      <Header />
-      {newEntry ? (
-        <VideoList videoList={newData} handleNewData={handleNewData} />
-      ) : (
-        <VideoList videoList={videoData} handleNewData={handleNewData} />
-      )}
-    </div>
-  );
+  function getChild() {
+    switch (videosLoadCall.state) {
+      case 'pending':
+        return (
+          <div className={styles.loading}>
+            <Icon size={2} path={mdiLoading} spin={true} />
+          </div>
+        );
+      case 'success':
+        return (
+          <>
+            <Header />
+            {newEntry ? (
+              <VideoList videoList={newData} handleNewData={handleNewData} />
+            ) : (
+              <VideoList videoList={videoData} handleNewData={handleNewData} />
+            )}
+          </>
+        );
+      case 'error':
+        return (
+          <div className={styles.error}>
+            <div>Nepodařilo se načíst data o třídě.</div>
+            <br />
+            <pre>{JSON.stringify(videosLoadCall.error, null, 2)}</pre>
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+
+  return <div className="App">{getChild()}</div>;
 }
 
 export default App;
